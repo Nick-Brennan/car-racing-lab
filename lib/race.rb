7230ffbe3e26d85f1a27a6913ac75603721a772a
Nick Brennan <nick_p_b@yahoo.com>
2015-09-17 10:52:55 -0700

    completed exercise

--- lib/race.rb
+++ lib/race.rb
@@ -1,5 +1,25 @@
 require_relative 'car'
 
 class Race
-  #write Race class code here
+	attr_accessor :cars
+  def initialize
+  	@cars = []
+  	@cars << Car.new << Car.new
+  	@cars[0].accelerate(rand(25...100))
+  	@cars[1].accelerate(rand(25...100))
+  end
+  def winner
+  	if cars[0].speed > cars[1].speed
+  		return cars[0]
+  	else
+  		return cars[1]
+  	end
+  end
+  def loser
+  	if cars[0].speed > cars[1].speed
+  		return cars[1]
+  	else
+  		return cars[0]
+  	end
+  end
 end
